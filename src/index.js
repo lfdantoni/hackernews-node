18267f65b731e05d19a4a04b8a01fba7ae61f229
lfdantoni <lfdantoni@gmail.com>
2019-11-23 18:58:46 -0300

    Added mutarions and link query by id

--- src/index.js
+++ src/index.js
@@ -1,22 +1,55 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-// 1
-const typeDefs = `
-type Query {
-  info: String!
-}
-`
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
+let idCount = links.length
 
-// 2
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: () => links,
+    link: (parent, args) => {
+      return links.find(link => link.id === args.id);
+    }
+  },
+  Mutation: {
+    post: (parent, args) => {
+       const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    },
+    updateLink(parent, args) {
+      const link = links.find(link => link.id === args.id);
+      if(link) {
+        link.description = args.description || link.description;
+        link.url = args.url || link.url;
+
+        return link;
+      }
+      return null;
+    },
+    deleteLink(parent, args) {
+      const linkIdx = links.findIndex(link => link.id === args.id);
+      if(linkIdx >= 0) {
+        const link = links[linkIdx];
+        links.splice(linkIdx, 1);
+
+        return link;
+      }
+      return null;
+    }
   }
 }
 
-// 3
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schemas/schema.graphql',
   resolvers,
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
